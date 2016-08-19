4a4c93a2f359e69b5c407ed74289252f30cb3960
Kurt Schneider <kurtms@live.com>
2016-08-19 01:28:18 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# kurtms.github.io
---
+++ index_script.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Kurt on 8/18/2016.
+ */
+
+function expandMenu(){
+    var sideBar = document.getElementById("sideBar");
+    sideBar.style.visibility = "visible";
+    var desiredWidth = 200; var rad = 0;
+    var animation = setInterval(animate, 3);
+    function animate(){
+        if(rad == 100)
+            clearInterval(animation);
+        rad++;
+        console.log(rad);
+        sideBar.style.width = desiredWidth * Math.sin(rad * Math.PI/180) + "px";
+    }
+}
+
+function closeMenu(){
+    var sideBar = document.getElementById("sideBar");
+    var desiredWidth = 200; var rad = 100;
+    var animation = setInterval(animate, 3);
+    function animate(){
+        if(rad == 0) {
+            clearInterval(animation);
+            sideBar.style.visibility = 'hidden';
+        }
+        rad--;
+        console.log(rad);
+        sideBar.style.width = desiredWidth * Math.sin(rad * Math.PI/180) + "px";
+    }
+
+}
